3911b43e74aeba6d5c0ffc269782858e37cbaa72
yangfan <wcatoo@outlook.com>
2023-12-12 02:52:49 +0000

    1

--- src/MessageQueue/MQRouter.cpp
+++ src/MessageQueue/MQRouter.cpp
@@ -1,7 +1,3 @@
-//
-// Created by 杨帆 on 2023/12/10.
-//
-
 #include "MQRouter.hpp"
 MQRouter::MQRouter(int tContext, const std::string &tAddress) {
   this->mContext = zmq::context_t(tContext);
--- src/MessageQueue/ReqMQ.cpp
+++ src/MessageQueue/ReqMQ.cpp
@@ -1,5 +1,3 @@
-//
-// Created by 杨帆 on 2023/12/9.
-//
+
 
 #include "ReqMQ.hpp"
