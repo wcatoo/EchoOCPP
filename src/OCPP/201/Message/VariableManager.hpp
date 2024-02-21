a013aa0f6640a751a3b25b4376569d44115a3140
wcatoo <wcatoo@outlook.com>
2024-02-22 07:32:03 +0800

    setVariable complete

--- src/OCPP/201/Message/VariableManager.hpp
+++ src/OCPP/201/Message/VariableManager.hpp
@@ -8,7 +8,7 @@ namespace OCPP201 {
 class VariableManager {
 private:
   std::filesystem::path mConfigurePath;
-  OCPP201::ComponentManager mComponentManager;
+  OCPP201::ComponentVariableManager mComponentManager;
 public:
   inline void setFilePath(const std::string &tPath) {
     this->mConfigurePath = tPath;
@@ -25,42 +25,6 @@ public:
 
   SetVariablesResponse setVariableRequestHandler(const SetVariablesRequest &tRequest) {
     SetVariablesResponse response;
-    for (SetVariableDataType dataRequest : tRequest.setVariableData) {
-      SetVariableResultType setVariableResultType;
-      setVariableResultType.setComponent(dataRequest.getComponent());
-      setVariableResultType.setVariable(dataRequest.getVariable());
-      setVariableResultType.setAttributeType(dataRequest.getAttributeType());
-      auto hasComponent = std::find_if(this->mComponentManager.components.begin(),
-                             this->mComponentManager.components.end(),
-                             [&dataRequest](const Component& component) {
-                               return component.componentName == dataRequest.getComponent().name;
-                             });
-      if (hasComponent != this->mComponentManager.components.end()) {
-        auto hasComponentInstance = std::find_if(hasComponent->component.begin(), hasComponent->component.end(),
-                                        [&dataRequest](ComponentInstance &componentInstance) {
-                                                   if (componentInstance.variableName == dataRequest.getVariable().name
-                                                       && componentInstance.componentInstance == dataRequest.getComponent().instance) {
-                                                     if (componentInstance.evse.has_value() && dataRequest.getComponent().evse.has_value()) {
-                                                       if (componentInstance.evse.value().id == dataRequest.getComponent().evse->id) {
-                                                         if (componentInstance.evse.value().connectorId.has_value() && dataRequest.getComponent().evse->connectorId.has_value()) {
-                                                           return componentInstance.evse.value().connectorId.value() == dataRequest.getComponent().evse->connectorId.value();
-                                                         }
-                                                       }
-                                                     }
-                                                   }
-                                                   return false;
-                                        });
-        if (hasComponentInstance != hasComponent->component.end()) {
-
-        } else {
-          setVariableResultType.setAttributeStatus(SetVariableStatusEnumType::UnknownVariable);
-        }
-      } else {
-        setVariableResultType.setAttributeStatus(SetVariableStatusEnumType::UnknownComponent);
-      }
-      response.setVariableResult.emplace_back(setVariableResultType);
-    }
-
     return response;
   }
 };
--- test/OCPP201ChargePointTest.cpp
+++ test/OCPP201ChargePointTest.cpp
@@ -13,46 +13,5 @@ protected:
 TEST_F(OCPP201ChargePoint, GetVariableRequest) {}
 
 TEST_F(OCPP201ChargePoint, SetVariablesRequest) {
-
-  std::string jsonString = R"(
-{
-  "components" : [
-    {
-      "componentName" : "OCPPCommCtrlr",
-      "component" : [
-        {
-          "componentInstance" : null,
-          "evse" : {
-            "id" : 1
-          },
-          "variableName" : "asd",
-          "variable" : [
-            {
-              "variableInstance" : "qwe",
-              "value" : [
-                {
-                  "attributeValue" : "asd123",
-                  "type" : "Actual"
-                }
-              ]
-            }
-          ]
-        }
-      ]
-    }
-  ]
-}
-)";
-  ComponentManager componentManager;
-  componentManager = nlohmann::json::parse(jsonString);
-  nlohmann::json tmp = componentManager;
-  std::cout << tmp.dump() << std::endl;
-
-
-  EVSEType evseType;
-
-  nlohmann::json json = evseType;
-  EVSEType tt = nlohmann::json::parse(json.dump());
-
 }
 }
